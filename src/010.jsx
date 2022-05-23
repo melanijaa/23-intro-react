8b475cdf53417df548659da8ea26db3a15d8762a
Melanija <melanija.sibakovska1@gmail.com>
2022-05-23 22:29:01 +0300

    new day

---
+++ src/010.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import './App.css';
+import randColor from './Functions/randColor';
+
+// const katinukai = ['Pilkis', 'Murkis', 'Rainis'];
+
+function App() {
+
+    const [spalva, setSpalva] = useState('yellow');
+    const [skaicius, setSkaicius] = useState(1);
+    const [kv, setKv] = useState([]);
+
+    // const mygtukas = () => {
+    //     console.log('Aš gražus mygtukas');
+    //     return mygtukoBrolis;
+    // }
+
+    // const mygtukoBrolis = () => {
+    //     console.log('Aš gražus mygtuko brolis');
+    // }
+
+    const beArgumentu = () => {
+        console.log('beArgumentu');
+    }
+
+    const suArgumentu = ka => {
+        console.log('suArgumentu: ' + ka);
+    }
+
+    const keistiSpalva = () => {
+
+        // const nauja = spalva === 'yellow' ? 'pink' : 'yellow'; BLOGAI!!!!!
+
+
+        setSpalva(senojiSpalva => senojiSpalva === 'yellow' ? 'pink' : 'yellow');
+
+        // console.log(spalva);
+
+    }
+
+    const addKv = () => {
+        setKv(k => [...k, randColor()]);
+    }
+
+    const remKv = () => {
+        setKv(k => k.slice(1));
+    }
+
+    const skaiciuotuvas = ka => setSkaicius(s => s + ka);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 style={{color: spalva}}>State {skaicius}</h1>
+                
+                <div className="kvc">
+                {
+                    kv.map((c, i) => <div className="kv" key={i} style={{backgroundColor:c}}>{i}</div>)
+                }
+                </div>
+
+
+
+                <button onClick={beArgumentu}>BE</button>
+                <button onClick={() => suArgumentu('labas')}>SU</button>
+                <button onClick={keistiSpalva}>Kita spalva</button>
+                <button onClick={() => skaiciuotuvas(1)}>+1</button>
+                <button onClick={() => skaiciuotuvas(-1)}>-1</button>
+                <button onClick={addKv}>ADD []</button>
+                <button onClick={remKv}>REMOVE []</button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
+//mygtukas()
+
+//const [A, B] = [1, 2];
+//const A = [1,2][0];
+//const B = [1,2][0];
+//state negalima keisti, pvz state++ yra labai blogai
