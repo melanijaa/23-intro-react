bf390ea3cffcac246b9dce074fe6e107c1aa2f89
Melanija <melanija.sibakovska1@gmail.com>
2022-05-24 12:38:53 +0300

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import './App.scss';
+import { useRef, useState } from "react"; //visi hugai turi buti importuoti
+
+//https://dmitripavlutin.com/react-useref-guide/
+
 function App() {
+    const [count, setCount] = useState(0);
+    const mano = useRef(0);
+    const panda = useRef(0);
+
+    const add = () => {
+    setCount(c => c + 1)
+    mano.current = mano.current + 3;
+    console.log(mano.current);
+    //const p = document.querySelector('#panda');
+    const p = panda.current;
+    console.log(p.dataset.panda); //DATASET
+
+    }
   return (
     <div className="App">
       <header className="App-header">
-      <h1>useRef LovalStorage</h1>
+      <h1>useRef LovalStorage {count}</h1>
+      <button onClick={add}>+1</button>
+      <div ref = {panda} data-panda = 'miega'></div>
       </header>
     </div>
   );
 }
 
 export default App;
+
