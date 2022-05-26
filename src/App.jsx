dac27e14ee9c7ce10a6f3a379c9e2e7f198f2e85
Melanija <melanija.sibakovska1@gmail.com>
2022-05-26 12:47:27 +0300

    avys

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.scss';
+import randColor from './Functions/randColor';
+import {v4 as uuidv4} from 'uuid';
+
+function App() {
+    const [avys, setAvys] = useState([]);
+    const newA = () => {
+        const avis = 
+        {
+            id: uuidv4(),
+            color:randColor(),
+            where: 'ganykla'
+        }
+        setAvys(a => [...a, avis]);
+    }
+
+    const go = id => {
+        setAvys(a => a.map(avis => avis.id === id ? {...avis, where: 'kirpykla'} : avis));
+    }
+
+    return (
+    <div className='App'>
+        <header className='App-header'>
+            <div className='kvc'>
+                {
+                    avys.filter(a => a.where === 'kirpykla').map(a => <div key={a.id} className='kv' style={{background:a.color}}></div>) //be "style" butu vienos spalvos
+                }
+            </div>
+            <h1>AVYS</h1>
+            <div className='kvc'>
+                {
+                    avys.filter(a => a.where === 'ganykla').map(a => <div onClick={() => go(a.id)} key={a.id} className='kv' style={{background:a.color}}></div>) //be "style" butu vienos spalvos
+                }
+            </div>
+            <button onClick={newA}>Naujas Avinas</button>
+        </header>
+    </div>
+    )
+    
+    
+}
+
+export default App;
