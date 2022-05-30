19f1bb79c821441feb40a08213730a2fe8f7e4be
Melanija <melanija.sibakovska1@gmail.com>
2022-05-30 12:50:27 +0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// duomenų kaupimas
+// arrray 
+// masyvas objektas
+// [{id=1 petras}]
+// [{id=2 antanas}]
+// [{id=3 bebras}]
+// indetikuojame pagal id, nes array index neturi reiksmes
+// id prasideda nuo 1
+// ID NEGALIMA PERPANAUDOTI
+//CRUD (Create, read, update and delete)
+
+import './App.scss';
+
+function App() {
+
+    return (
+    <div className='App'>
+        <header className='App-header'  style={{background: color}}>
+  
+        </header>
+    </div>
+    )
+    
+}
+
+export default App;
