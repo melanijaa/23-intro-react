7154b66f2ffc45272b0473b885ecdf034b2d9203
Melanija <melanija.sibakovska1@gmail.com>
2022-05-30 12:51:27 +0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
     return (
     <div className='App'>
-        <header className='App-header'  style={{background: color}}>
+        <header className='App-header'>
   
         </header>
     </div>
