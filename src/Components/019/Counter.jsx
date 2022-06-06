275ce9fd01cdd80bd87b014906548fc988eb9ca9
Melanija <melanija.sibakovska1@gmail.com>
2022-06-06 14:00:47 +0300

    svg

--- src/Components/019/Counter.jsx
+++ src/Components/019/Counter.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React from 'react';
+
 
 class Counter extends React.Component {
-
     constructor() {
         super();
         this.state = {counter: 0};
     }
 
-    doClick = () => {
+    doCklick = () => {
         this.setState(s => ({counter: s.counter + 1}));
+    }
+
+    componentDidMount() {
+        console.log('LOaded');
+    }
+
+    componentWillUnmount() {
+        console.log('numiro');
+    }
+
+    componentDidUpdate() {
+        console.log('apsiapdeitino');
     }
 
     render() {
         return (
             <>
             <div>{this.state.counter}: {this.props.when}</div>
-            <button onClick={(this.doClick)}>GO +1</button>
+            <button onClick={this.doCklick}>
+            <svg>
+                <use href="#star"/>
+            </svg>
+            <svg className="kitas-svg">
+                <use href="#star"/>
+            </svg>
+            <svg>
+                <use href="#star"/>
+            </svg>
+            </button>
             </>
         )
     }
