55c6e936bbab0ecea03c7334d44c41d751416532
Melanija <melanija.sibakovska1@gmail.com>
2022-06-10 12:45:31 +0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,76 @@
-import './App.scss';
-
+import { useEffect, useState } from 'react';
+import './bootstrap.css';
+import './crud.scss';
+import Create from './Components/CRUD/Create';
+import List from './Components/CRUD/List';
+import { create, edit, read, remove } from './Functions/localStorage';
+import Edit from './Components/CRUD/Edit';
+// import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-    </div>
-  );
+    const [lastUpdate, setLastUpdate] = useState(Date.now());
+
+    const [exes, setExes] = useState(null);
+    const [modalData, setModalData] = useState(null);
+
+    const [createData, setCreateData] = useState(null);
+    const [deleteData, setDeleteData] = useState(null);
+    const [editData, setEditData] = useState(null);
+
+    //Read
+    useEffect(() => {
+        setExes(read());
+    }, [lastUpdate]);
+
+    // Create
+    useEffect(() => {
+        if (null === createData) {
+            return;
+        }
+        create(createData);
+        setLastUpdate(Date.now());
+
+    }, [createData]);
+
+    // Delete
+    useEffect(() => {
+        if (null === deleteData) {
+            return;
+        }
+        remove(deleteData);
+        setLastUpdate(Date.now());
+
+    }, [deleteData]);
+
+    // Edit
+    useEffect(() => {
+        if (null === editData) {
+            return;
+        }
+        edit(editData);
+        setLastUpdate(Date.now());
+
+    }, [editData]);
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <Create setCreateData={setCreateData}></Create>
+                    </div>
+                    <div className="col-8">
+                        <List exes={exes} setDeleteData={setDeleteData} setModalData={setModalData}></List>
+                    </div>
+                </div>
+            </div>
+            <Edit setEditData={setEditData} modalData={modalData} setModalData={setModalData}></Edit>
+        </>
+    );
+
+
 }
-
 export default App;
+
+//ctrl + h
